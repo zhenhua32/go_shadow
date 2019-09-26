dd16b065be7ee849d5348fdd637682075e8728f0
tzh <2375626546@qq.com>
2019-09-26 17:49:52 +0800

    save

--- Cipher/aes.go
+++ Cipher/aes.go
@@ -1,4 +1,35 @@
 package cipher
 
+import (
+	"crypto/aes"
+	gocipher "crypto/cipher"
+
+	"golang.org/x/crypto/scrypt"
+)
+
 type AESCrypto struct {
+	key   []byte
+	block gocipher.Block
+}
+
+// NewAESCrypto 创建一个新的 AESCrypto
+// keylen 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256.
+func NewAESCrypto(password string, keylen int) *AESCrypto {
+	key, _ := GenKey(password, keylen)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	return &AESCrypto{
+		key:   key,
+		block: block,
+	}
+
+}
+
+// GenKey 从密码中生成 derived key
+func GenKey(password string, keylen int) ([]byte, error) {
+	dk, err := scrypt.Key([]byte(password), []byte("fake"), 32768, 8, 1, keylen)
+	return dk, err
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/sirupsen/logrus v1.4.2
+	golang.org/x/crypto v0.0.0-20190923035154-9ee001bba392
 	golang.org/x/sys v0.0.0-20190924154521-2837fb4f24fe // indirect
 )
