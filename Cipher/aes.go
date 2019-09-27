c76b3f322159ab4fdf5e68cfa13adc372d903a5e
tzh <2375626546@qq.com>
2019-09-27 10:20:08 +0800

    实现 aes cfb 加密

--- Cipher/aes.go
+++ Cipher/aes.go
@@ -3,33 +3,66 @@ package cipher
 import (
 	"crypto/aes"
 	gocipher "crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"io"
 
 	"golang.org/x/crypto/scrypt"
 )
 
+// AESCrypto 是 AES加密实现, 暂时是 CFB
 type AESCrypto struct {
-	key   []byte
-	block gocipher.Block
+	key   []byte         // key 是从密码中生成 derived key
+	block gocipher.Block // block 是 cipher.Block, 块加密
 }
 
 // NewAESCrypto 创建一个新的 AESCrypto
 // keylen 16, 24, or 32 bytes to select AES-128, AES-192, or AES-256.
-func NewAESCrypto(password string, keylen int) *AESCrypto {
-	key, _ := GenKey(password, keylen)
+func NewAESCrypto(password string, keylen int) (*AESCrypto, error) {
+	key, err := GenKey(password, keylen)
+	if err != nil {
+		return nil, err
+	}
 	block, err := aes.NewCipher(key)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 
 	return &AESCrypto{
 		key:   key,
 		block: block,
-	}
-
+	}, nil
 }
 
 // GenKey 从密码中生成 derived key
 func GenKey(password string, keylen int) ([]byte, error) {
 	dk, err := scrypt.Key([]byte(password), []byte("fake"), 32768, 8, 1, keylen)
 	return dk, err
+}
+
+// EncodeData 使用 cfb 加密
+func (c *AESCrypto) EncodeData(data []byte) ([]byte, error) {
+	ciphertext := make([]byte, aes.BlockSize+len(data))
+	iv := ciphertext[:aes.BlockSize]
+	// 随机填充 iv
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return nil, err
+	}
+	stream := gocipher.NewCFBEncrypter(c.block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], data)
+	return ciphertext, nil
+}
+
+// DecodeData 使用 cfb 解密
+func (c *AESCrypto) DecodeData(data []byte) ([]byte, error) {
+	if len(data) < aes.BlockSize {
+		return nil, errors.New("ciphertext too short")
+	}
+	iv := data[:aes.BlockSize]
+	ciphertext := data[aes.BlockSize:]
+	plaintext := make([]byte, len(ciphertext))
+
+	stream := gocipher.NewCFBDecrypter(c.block, iv)
+	stream.XORKeyStream(plaintext, ciphertext) // in-place work
+	return plaintext, nil
 }
---
+++ Cipher/aes_test.go
@@ -0,0 +1,50 @@
+package cipher
+
+import (
+	"crypto/aes"
+	"encoding/hex"
+	"testing"
+)
+
+var password = "hello"
+
+func TestNewAESCrypto(t *testing.T) {
+	c, err := NewAESCrypto(password, 16)
+	if err != nil {
+		t.Errorf("运行 NewAESCrypto 失败: %v", err)
+	}
+	if c.key == nil {
+		t.Error("key 是空的")
+	}
+	if c.block == nil {
+		t.Error("block 是空的")
+	}
+}
+
+func TestAESCrypto_EncodeData(t *testing.T) {
+	c, _ := NewAESCrypto(password, 16)
+	plaintext := []byte("hello world")
+	ciphertext, err := c.EncodeData((plaintext))
+	if err != nil {
+		t.Errorf("AESCrypto 加密时发生错误: %v", err)
+	}
+	if len(ciphertext) != len(plaintext)+aes.BlockSize {
+		t.Error("加密后的大小不对")
+	}
+	// 12516deaf8a06cc4ff9072fb84938e87b557c29d8017274406f7f5
+	t.Log(hex.EncodeToString(ciphertext))
+}
+
+func TestNewAESCrypto_DecodeData(t *testing.T) {
+	c, _ := NewAESCrypto(password, 16)
+	ciphertext, _ := hex.DecodeString("12516deaf8a06cc4ff9072fb84938e87b557c29d8017274406f7f5")
+	plaintext, err := c.DecodeData(ciphertext)
+	if err != nil {
+		t.Errorf("AESCrypto 解密时发生错误: %v", err)
+	}
+	source := "hello world"
+	if string(plaintext) != source {
+		t.Errorf("解密后的结果不正确, 解密结果为 %s, 应该是 %s", string(plaintext), source)
+	}
+	t.Log(string(plaintext))
+}
